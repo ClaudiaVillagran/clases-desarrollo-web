2c5407f7ddc26b85bf5af981112439d4ab59a7c7
Claudia Villagran <claudia.villagran1901@alumnos.ubiobio.cl>
2025-08-25 23:44:12 -0400

    componentes mui

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ComponenteBoton } from './ComponenteBoton'
-import BasicButtonComponent from './BasicButtonComponent'
+import BotonBasico from './BotonBasico'
 
 function App() {
 
   return (
     <div>
       <h1>Zapatería</h1>
-      <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
+      <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap', backgroundColor: '#555'}}>
         <div class='contenedor_producto_1' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }}>
           <div style={{ width: "40%", height: "200px" }}>
             <img style={{ width: "100%", height: "100%" }} src="https://shop.mango.com/assets/rcs/pics/static/T7/fotos/S/77043269_78.jpg?imwidth=2048&imdensity=1&ts=1716291248140" />
@@ -19,7 +19,7 @@ function App() {
             <h2>$20000</h2>
             <p>Este zapato cuenta con un diseño innovador, cómodo y de alta gama</p>
             {/* <button style={{ backgroundColor: "#322" }}>Comprar</button> */}
-            <ComponenteBoton nombreBoton={'Aprovecha tu descuento'} />
+            <BotonBasico disabled={'false'} />
           </div>
         </div>
         <div class='contenedor_producto_2' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }}>
@@ -30,7 +30,7 @@ function App() {
             <h2>$40000</h2>
             <p>Esta es una zapatilla deportiva, utilizada para trakking</p>
 
-            <BasicButtonComponent/>
+ 
           </div>
 
         </div>
@@ -42,7 +42,7 @@ function App() {
           <div style={{ width: "60%", height: "200px" }}>
             <h2>$15000</h2>
             <p>Este zapato esta en oferta por lanzamiento</p>
-            <ComponenteBoton />
+            <ComponenteBoton disabled={'true'} />
           </div>
         </div>
       </div>
---
+++ src/BotonBasico.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+export default function BotonBasico({isDisabled}) {
+  return (
+    <Stack direction="row" spacing={2}>
+        
+
+        {}
+      <Button variant="contained">Contained</Button>
+
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+
+    </Stack>
+  );
+}
