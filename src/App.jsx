47604355d8853e27433d4ab138fb26dd2bf458cc
Claudia Villagran <claudia.villagran1901@alumnos.ubiobio.cl>
2025-09-03 20:27:12 -0400

    componente carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { ComponenteBoton } from './ComponenteBoton'
 import BotonBasico from './BotonBasico'
+import CarritoCompras from './CarritoCompras'
 
 function App() {
 
   return (
     <div>
+
       <h1>Zapatería</h1>
-      <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap', backgroundColor: '#555'}}>
-        <div class='contenedor_producto_1' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }}>
+      <CarritoCompras/>
+      <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap', backgroundColor: '#555' }}>
+        <div className='contenedor_producto_1' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }}>
           <div style={{ width: "40%", height: "200px" }}>
             <img style={{ width: "100%", height: "100%" }} src="https://shop.mango.com/assets/rcs/pics/static/T7/fotos/S/77043269_78.jpg?imwidth=2048&imdensity=1&ts=1716291248140" />
           </div>
-          <div style={{ width: "60%", height: "200px" }}>
+          <div style={{ width: "60%", height: "200px", display:'flex', flexDirection: 'column', alignItems:'center'}}>
             <h2>$20000</h2>
             <p>Este zapato cuenta con un diseño innovador, cómodo y de alta gama</p>
             {/* <button style={{ backgroundColor: "#322" }}>Comprar</button> */}
-            <BotonBasico disabled={'false'} />
+            <BotonBasico isDisabled={false} />
           </div>
         </div>
-        <div class='contenedor_producto_2' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }}>
+        <div className='contenedor_producto_2' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }}>
           <div style={{ width: "40%", height: "200px" }}>
             <img style={{ width: "100%", height: "100%" }} src="https://shop.mango.com/assets/rcs/pics/static/T7/fotos/S/77043269_78.jpg?imwidth=2048&imdensity=1&ts=1716291248140" />
           </div>
-          <div style={{ width: "60%", height: "200px" }}>
+          <div style={{ width: "60%", height: "200px",  display:'flex', flexDirection: 'column', alignItems:'center' }}>
             <h2>$40000</h2>
             <p>Esta es una zapatilla deportiva, utilizada para trakking</p>
-
- 
+            <BotonBasico isDisabled={false} />
           </div>
 
         </div>
-        <div class='contenedor_producto_3' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }} >
+        <div className='contenedor_producto_3' style={{ display: 'flex', border: '2px solid #acb', flex: '1 1 360px', }} >
           <div style={{ width: "40%", height: "200px" }}>
             <img style={{ width: "100%", height: "100%" }} src="https://shop.mango.com/assets/rcs/pics/static/T7/fotos/S/77043269_78.jpg?imwidth=2048&imdensity=1&ts=1716291248140" />
 
           </div>
-          <div style={{ width: "60%", height: "200px" }}>
+          <div style={{ width: "60%", height: "200px", display:'flex', flexDirection: 'column', alignItems:'center'}}>
             <h2>$15000</h2>
             <p>Este zapato esta en oferta por lanzamiento</p>
-            <ComponenteBoton disabled={'true'} />
+            <BotonBasico isDisabled={true} />
           </div>
         </div>
       </div>
--- src/BotonBasico.jsx
+++ src/BotonBasico.jsx
@@ -2,18 +2,17 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-export default function BotonBasico({isDisabled}) {
+export default function BotonBasico({ isDisabled }) {
+
   return (
-    <Stack direction="row" spacing={2}>
-        
-
-        {}
-      <Button variant="contained">Contained</Button>
-
-      <Button variant="contained" disabled>
-        Disabled
-      </Button>
-
+    <Stack direction="row" spacing={2} alignItems='center'>
+      {isDisabled ?
+        <Button variant="contained" disabled>
+          Agotado
+        </Button>
+      :
+      <Button variant="contained">Comprar</Button>
+    }
     </Stack>
   );
 }
